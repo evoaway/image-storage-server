433a40b896a25617ec7a28803dd292f95a3d22a6
evoaway <anatolii.mandzynets@gmail.com>
2024-05-22 15:05:23 +0300

    Add user update

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,10 +29,11 @@ class UserController {
             if (resources.length > 0) {
                 return res.status(400).json({status: 'error', message: "User already exists" });
             }
-            const hashPassword = await bcrypt.hash(password, 5)
-            const newBody = {email, hashPassword, firstname, lastname}
+            const hashedPassword = await bcrypt.hash(password, 5)
+            const newBody = {email, hashPassword:hashedPassword, firstname, lastname}
             const { resource } = await container.items.create(newBody);
-            return res.status(200).json({status: 'success', user: resource});
+            const {hashPassword, ...userData} = resource
+            return res.status(200).json({status: 'success', user: userData});
         } catch (e) {
             res.status(500).json({status: 'error', message: e.message})
         }
@@ -67,6 +68,7 @@ class UserController {
             const userId = req.user.id;
             const container = await getContainer('Users');
             const { resource:user } = await container.item(userId).read();
+            const {hashPassword, ...userData} = user
             const querySpec = {
                 query: 'SELECT SUM(c.size) FROM c WHERE c.userId = @userId',
                 parameters: [
@@ -78,7 +80,38 @@ class UserController {
             };
             const imageContainer = await getContainer('Images');
             const { resources } = await imageContainer.items.query(querySpec).fetchAll();
-            return res.status(200).json({ status:'success', user: user, memory:resources[0] });
+            return res.status(200).json({ status:'success', user: userData, memory:resources[0] });
+        } catch (e) {
+            res.status(500).json({status: 'error', message: e.message})
+        }
+    }
+    async updateMyInfo(req,res) {
+        try {
+            const userId = req.user.id;
+            const container = await getContainer('Users');
+            const { resource: user } = await container.item(userId).read();
+            const body = req.body
+            console.log(body)
+            if(body.email){
+                const querySpec = {
+                    query: 'SELECT c.id FROM c WHERE c.email = @email',
+                    parameters: [
+                        {
+                            name: '@email',
+                            value: body.email
+                        }
+                    ]
+                };
+                const { resources } = await container.items.query(querySpec).fetchAll();
+                if (resources.length > 0) {
+                    return res.status(400).json({status: 'error', message: "User already exists" });
+                }
+            }
+            Object.keys(body).forEach(key => {
+                user[key] = body[key];
+            });
+            const { resource: updatedUser } = await container.item(userId).replace(user);
+            return res.status(200).json({ status:'success', user: updatedUser });
         } catch (e) {
             res.status(500).json({status: 'error', message: e.message})
         }
